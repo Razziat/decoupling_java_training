d9dd2344f076913619bf93e46f90cb25b19ac89c
henri.thomas <henri.thomas@epita.fr>
2023-03-30 17:10:31 +0200

    partie 3

---
+++ src/main/java/fr/lernejo/guessgame/HumanPlayer.java
@@ -0,0 +1,20 @@
+package fr.lernejo.guessgame;
+
+import fr.lernejo.logger.*;
+
+import java.util.Scanner;
+
+public class HumanPlayer implements Player{
+    public final Logger logger = LoggerFactory.getLogger("Player");
+    public Scanner console = new Scanner(System.in);
+
+    @Override
+    public long askNextGuess() {
+        return 0;
+    }
+
+    @Override
+    public void respond(boolean lowerOrGreater) {
+
+    }
+}
